ab81d93f692a9f6f72d0dd1c7e758c74c756d4b7
lyoshi <380036805@qq.com>
2018-05-20 19:18:19 +0800

    update

--- Homework_2/PlotShearEnvelope.m
+++ Homework_2/PlotShearEnvelope.m
@@ -1,13 +1,15 @@
 function PlotShearEnvelope(mode,U,kk)
 [~,n]=size(U);
-x=zeros(n,1);
-y=zeros(n,1);
+x=zeros(2*n,1);
+y=zeros(2*n,1);
 scale=1;
 for j=1:n % ÷ µ„
     d=InterStoreyDrift(j,mode,U);
     shear=kk(j)*d;
-    x(j)=max(abs(shear));
-    y(j)=(j-1)*scale;
+    x(2*j-1)=max(abs(shear));
+    y(2*j-1)=(j-1)*scale;
+    x(2*j)=x(2*j-1);
+    y(2*j)=j*scale;
 end
 plot(x,y);
 daspect([1 1e-4 1]);
--- Homework_2/main_systhesis_earthquake.m
+++ Homework_2/main_systhesis_earthquake.m
@@ -1,4 +1,4 @@
-% 合成第一条地震波：二类场地，第一分组，多遇，烈度7
+%% 合成第一条地震波：二类场地，第一分组，多遇，烈度7
 clc;clear;
 wg=17.95;dg=0.72;t1=0.8;t2=7;c=0.35;
 [a,t,Sx,w]=CloughPenzien(wg,dg,t1,t2,c);
@@ -11,4 +11,16 @@ plot(w,Sx);
 xlabel('频率w（Hz）');
 ylabel('功率谱密度Sx（m^2/s^3）');
 title('Clough-Penzien谱功率谱1');
-% 合成第二条地震波：三类场地，第一分组，多遇，烈度7
+%% 合成第二条地震波：三类场地，第一分组，多遇，烈度7
+clc;clear;
+wg=13.96;dg=0.8;t1=1.2;t2=9;c=0.25;
+[a,t,Sx,w]=CloughPenzien(wg,dg,t1,t2,c);
+plot(t,a);
+xlabel('时间t（s）');
+ylabel('加速度a（m/s^2）');
+title('人工合成地震加速度时程2');
+figure
+plot(w,Sx);
+xlabel('频率w（Hz）');
+ylabel('功率谱密度Sx（m^2/s^3）');
+title('Clough-Penzien谱功率谱2');
