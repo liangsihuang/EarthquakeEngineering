37332b366d0e4b95d19cdd44525d19b107c0067c
lyoshi <380036805@qq.com>
2018-05-20 16:38:37 +0800

    backup

---
+++ Homework_2/CloughPenzien.m
@@ -0,0 +1,54 @@
+function [a,t,Sx,w]=CloughPenzien(wg,dg,t1,t2,c)
+% 输入：
+% 场地特征频率wg
+% 场地阻尼比dg (ground damping ratio)
+% 强度包络函数计算参数：
+% t1,t2:主振平稳段的首末时间
+% c：一个用于计算g的经验参数？
+% 输出：
+% 地震加速度时程a
+% 过滤系统参数 filter
+wf=0.1*wg;
+df=dg;
+dw=0.2;
+w=0:dw:99.8;
+N=length(w);
+phi=rand(N,1)*2*pi;% 相位角为0-2×pi之间均匀分布的随机变量
+t=0:0.01:40; % 地震波持续40s
+nt=length(t);
+a=zeros(nt,1);
+Sx=zeros(N,1);
+for k=1:N
+    Sx(k)=PowerSpectralDensity(w(k),wg,dg,wf,df);
+end
+for i=1:nt
+    g=IntensityEnvelope(t1,t2,c,t(i));
+    for j=1:N
+        S=g^2*Sx(j);
+        A=sqrt(4*S*dw);
+        a(i)=a(i)+A*cos(w(j)*t(i)-phi(j));
+    end
+end
+    
+
+
+function Sx=PowerSpectralDensity(w,wg,dg,wf,df)
+% 根据Clough-Penzien模型
+numerator=wg^4+4*dg^2*wg^2*w^2;% 分子
+denominator=wg^2-w^2+4*dg^2*wg^2*w^2*(wf^2-w^2)+4*df^2*wf^2*w^2;% 分母
+S0=3; %谱强度因子，为什么取3？？？
+Sx=numerator/denominator*S0;
+end
+
+function g=IntensityEnvelope(t1,t2,c,t)
+if (t<=t1)
+    g=(t/t1)^2;
+else if (t<=t2)
+        g=1;
+    else
+        g=exp(-c*(t-t2));
+    end
+end
+end
+
+end
---
+++ Homework_2/main_systhesis_earthquake.m
@@ -0,0 +1,14 @@
+% 合成第一条地震波：二类场地，第一分组，多遇，烈度7
+clc;clear;
+wg=17.95;dg=0.72;t1=0.8;t2=7;c=0.35;
+[a,t,Sx,w]=CloughPenzien(wg,dg,t1,t2,c);
+plot(t,a);
+xlabel('时间t（s）');
+ylabel('加速度a（m/s^2）');
+title('人工合成地震加速度时程1');
+figure
+plot(w,Sx);
+xlabel('频率w（Hz）');
+ylabel('功率谱密度Sx（m^2/s^3）');
+title('Clough-Penzien谱功率谱1');
+% 合成第二条地震波：三类场地，第一分组，多遇，烈度7
